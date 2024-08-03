d6ddfef36f051e5b5e5d8a9dd7cbaf30a8000259
comanda316 <44969930+comanda316@users.noreply.github.com>
2024-08-04 00:00:06 +0400

    Add files via upload

--- lang/en.php
+++ lang/en.php
@@ -1,1 +1,23 @@
-
+<?php
+$lang = array(
+    "welcome" => "Welcome",
+    "home" => "Home",
+    "about" => "About",
+    "services" => "Services",
+    "contact" => "Contact",
+    "register" => "Register",
+    "login" => "Login",
+    "logout" => "Logout",
+    "username" => "Username",
+    "password" => "Password",
+    "first_name" => "First Name",
+    "last_name" => "Last Name",
+    "phone" => "Phone Number",
+    "address" => "Address",
+    "personal_id" => "Personal ID",
+    "submit" => "Submit",
+    "dashboard" => "Dashboard",
+    "role" => "Role",
+    "language" => "Language" // Добавляем перевод для "Язык"
+);
+?>
